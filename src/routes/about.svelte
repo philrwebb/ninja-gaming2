86eb58e22a06ce1deed3b4915cbbbd89335bb915
philrwebb <prwebb@optusnet.com.au>
2022-01-24 11:34:40 +0800

    initial checkin

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<div class="about">
+  <h2>About Us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+    laboriosam enim repellendus modi consequuntur saepe nihil at natus?
+    Cupiditate iusto dolorem tenetur doloremque ipsum saepe est excepturi sint!
+    Ab, cum non placeat ipsum, et explicabo aliquam quam ut facere dolor vero
+    provident consectetur exercitationem. Laboriosam vel non quia labore autem.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas commodi
+    non eaque aut dolorem, iure veritatis voluptatem neque atque ex dolores
+    beatae quos aliquid debitis culpa sit sapiente, dolorum numquam et harum?
+    Culpa cumque eius animi esse ex veritatis commodi rerum pariatur est
+    eligendi nemo minus tempora ducimus sit consectetur explicabo, tempore eum
+    accusantium. Dignissimos quo officia cumque error veniam ab molestias animi
+    pariatur? Rerum doloremque atque illo dolor perferendis, aliquam dicta
+    tenetur molestias delectus similique minus nam. Molestias enim dicta earum
+    quaerat incidunt sint porro! Ullam atque ducimus officia, a corrupti cum,
+    odit voluptatibus, nisi consectetur saepe delectus animi?
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
